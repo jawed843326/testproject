b4e80e297a8ed57d7bffc53da862b4557c15fff0
736312 <736312@LTIN213489.cts.com>
2020-05-18 14:20:57 +0530

    I have just added a class to print hello

---
+++ MyFirstProjectOnGit/src/com/myfirstprojectongit/PrintHelloClass.java
@@ -0,0 +1,11 @@
+package com.myfirstprojectongit;
+
+public class PrintHelloClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, This is your first project hosted on git repo.");
+	}
+
+}
